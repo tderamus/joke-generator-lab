45a9e10a048bd228170234b070aeca1042bbb290
tderamus <deramust@gmail.com>
2024-10-12 23:10:38 -0500

    retry commit with all saved files

---
+++ src/components/hooks/page.js
@@ -0,0 +1,20 @@
+// 'use client'
+
+// import React, { useEffect, useState } from 'react'
+
+// export default function Hooks() {
+//   const [stateHook, setStateHook] = useState('State Hook Default Value');
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setStateHook('The useEffect Loaded Me!');
+//     }, 5000);
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>This is a use effect hook page</h1>
+//     </div>
+//   );
+// }
